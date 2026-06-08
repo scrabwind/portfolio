3ceee274920ddefb1d03e3e1d45537c3a0222c31
Kamil Wiatrowski <scrabwind@gmail.com>
2026-06-08 19:13:57 +0200

    fix: add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://scrabwind.github.io/",
+  base: "/portfolio",
   i18n: {
     locales: ["en", "pl"],
     defaultLocale: "en",
