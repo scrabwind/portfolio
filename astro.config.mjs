a26ecfed353d621763a1dc33202eac2794310a19
Kamil Wiatrowski <scrabwind@gmail.com>
2026-06-08 19:02:42 +0200

    feat: add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://scrabwind.github.io/",
   i18n: {
     locales: ["en", "pl"],
     defaultLocale: "en",
